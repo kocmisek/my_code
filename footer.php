2e15c0a58f42c4fce9b3746cd7bad6b2aba259ae
kocmisek <81322388+kocmisek@users.noreply.github.com>
2022-07-19 15:18:20 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,44 @@
+
+<footer>
+
+<a href="https://pureweb.cz"><p style="text-align:center; margin-bottom:0">©<?php echo date("Y");?>&nbsp;pureweb.cz
+        </a></p>
+<p style="text-align:center; margin: 0;
+">Všechna práva vyhrazena.</p>
+</footer>
+
+
+<!-- LAnguange -->
+<script>
+
+</script>
+
+<!-- Animate js -->
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="/wp-content/themes/pureweb/js/animate.js"></script>
+
+<!-- Universal parallax -->
+<script src="/wp-content/themes/pureweb/js/universal-parallax.min.js"></script>
+<script src="/wp-content/themes/pureweb/js/universal_parallax.js"></script>
+
+<!-- Smoot scroll--
+<script src="/wp-content/themes/pureweb/js/smoot_scroll.js"></script>-->
+
+<!-- MENU script -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
+<script src="/wp-content/themes/pureweb/js/menu.js"></script>
+
+<!-- MOBILE MENU script-->
+<script src="/wp-content/themes/pureweb/js/mob_menu.js"></script>
+
+<!-- Projects script -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/2.2.2/isotope.pkgd.min.js"></script>
+<script src="/wp-content/themes/pureweb/js/projects.js"></script>
+
+<!-- Read more -->
+<script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+<script src="/wp-content/themes/pureweb/js/read_more.js"></script>
+
+</body>
+</html>

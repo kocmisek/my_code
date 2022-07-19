e6d9025e1571e2c7bfdbca6bccefb74f86ecd86a
kocmisek <81322388+kocmisek@users.noreply.github.com>
2022-07-19 15:16:27 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,330 @@
+<?php get_header() ?>
+
+<section  id="home" class="container--full home">
+	<div class="box">
+	<!-- LOGO -->
+		<div class="logo"><img src="/wp-content/themes/pureweb/logo/logo_pureweb03.png" alt="Pureweb - Martina Kocmanová"></div>
+	<!-- END logo-->
+
+<!-- INTRO -->
+		<div class="flex">
+				<div class="intro">
+					<div class="text">
+						<h1 id="animate"><img src="/wp-content/themes/pureweb/img/name.svg" alt="Martina Kocmanová"></h1>
+						<h2>kreativní technolog propojující webový design a vývoj</h2> 
+						<h2>partner pro vaši webovou prezentaci</h2>
+						<h4>-- web designer & web developer</h4>
+					</div>
+
+				</div>
+				<div class="me">
+					<img src="/wp-content/themes/pureweb/img/me_intro.png" alt="Martina Kocmanová">
+				</div>
+		</div>
+	</div>
+		<div class="parallax bg" data-parallax-image="/wp-content/themes/pureweb/img/the-creative-exchange-d2zvqp3fpro-unsplash.jpg"></div>
+	</section>
+<!-- END intro-->
+
+<!-- QUOTE -->
+
+<section id="quote">
+	<div class="info quote">
+		<figure class="parallax">
+			<blockquote>
+			<p class="quote" style="text-align: center;">“Méně je více..”</p>
+			</blockquote><figcaption>– Ludwig Mies van der Rohe</figcaption>
+		</figure>
+	</div>
+</section>
+<!-- END quote -->
+
+<!-- PROJECTS -->
+<section id="projekty">
+<div id="top"></div>
+<div class="overlay">
+	<div class="articles-navigation">
+	  <div class="close">
+		<div></div>
+		<div></div>
+	  </div>
+	</div>
+  </div>
+  <div class="wrapper" class="parallax">
+	
+
+	<section class="gallery">
+			<div class="gallery-box">
+				<div class="grey_bg_box"></div>
+				<div class="side_title_letter"><h1>P</h1></div>
+				<div class="side_title_text"><h1>PROJEKTY</h1></div>
+
+				<div class="item" data-article="article-1"><p>ava barre</p><a href="#avabarre"><img src="/wp-content/themes/pureweb/projects/gray.jpg"><div class="desc"><img src="/wp-content/themes/pureweb/projects/avabarre_view.jpg"/></div></a></div>
+				<div class="item" data-article="article-2"><p>vaše ortopedie</p>
+					<a href="#vaseortopedie">
+						<picture>
+							<source media="(max-width: 383px)" srcset="/wp-content/themes/pureweb/projects/gray.jpg">
+							<source media="(max-width: 582px)" srcset="/wp-content/themes/pureweb/projects/2.jpg">
+							<img src="/wp-content/themes/pureweb/projects/2.jpg"/>
+						</picture>
+						<div class="desc">
+							<img src="/wp-content/themes/pureweb/projects/vaseortopedie_view.jpg"/>
+						</div>
+					</a>
+				</div>
+				<div class="item" data-article="article-3"><p>studio lenky krejčové</p>
+					<a href="#studiolenkykrejcove">
+						<picture>
+							<source media="(max-width: 383px)" srcset="/wp-content/themes/pureweb/projects/gray.jpg">
+							<source media="(max-width: 582px)" srcset="/wp-content/themes/pureweb/projects/04.jpg">
+							<img src="/wp-content/themes/pureweb/projects/gray.jpg">
+						</picture>
+						<div class="desc">
+							<img src="/wp-content/themes/pureweb/projects/studio_lenky_krejcove_view.jpg"/>
+						</div>
+					</a>
+				</div>
+				<div class="item" data-article="article-4"><p>glasstyle</p>
+					<a href="#glasstyle">
+						<picture>
+							<source media="(max-width: 383px)" srcset="/wp-content/themes/pureweb/projects/2.jpg">
+							<source media="(max-width: 582px)" srcset="/wp-content/themes/pureweb/projects/05.jpg">
+							<img src="/wp-content/themes/pureweb/projects/04.jpg">
+						</picture>
+						<div class="desc">
+							<img src="/wp-content/themes/pureweb/projects/01_glasstyle.jpg"/>
+						</div>
+					</a>
+				</div>
+				<div class="item" data-article="article-5"><p>jakub komůrka</p>
+					<a href="#jakobukomurka">
+						<picture>
+							<source media="(max-width: 383px)" srcset="/wp-content/themes/pureweb/projects/05.jpg">
+							<source media="(max-width: 582px)" srcset="/wp-content/themes/pureweb/projects/07.jpg">
+							<img src="/wp-content/themes/pureweb/projects/05.jpg">
+						</picture>
+						<div class="desc">
+							<img src="/wp-content/themes/pureweb/projects/jakub_komurka_view.jpg"/>
+						</div>
+					</a>
+				</div>
+				<div class="item" data-article="article-6"><p>likema</p>
+					<a href="#likema">
+						<picture>
+							<source media="(max-width: 383px)" srcset="/wp-content/themes/pureweb/projects/08.jpg">
+							<source media="(max-width: 582px)" srcset="/wp-content/themes/pureweb/projects/08.jpg">
+							<img src="/wp-content/themes/pureweb/projects/gray.jpg">
+						</picture>
+						<div class="desc">
+							<img src="/wp-content/themes/pureweb/projects/likema_view.jpg"/>
+						</div>
+					</a>
+				</div>
+			</div>	  
+	</section>
+	<section class="project-view">
+	  <div class="project-container">
+		<article class="project-content">
+		  <section class="project-article-content"></section>
+		  <footer class="project-article-footer"></footer>
+		</article>
+	  </div>
+	</section>
+  </div>
+  
+  <section id="article-contents">
+	<div class="article-html" data-article="article-1">
+		<div class="project_info"> 
+			<h2>PROJEKT: ava barre | e-shop</h2>
+			<h2>ROK: 2021</h2>
+		</div>
+	  	<img src="/wp-content/themes/pureweb/projects/avabarre_02.png">
+		<div class="btn_info">
+			<input type="button" onclick="location.href='/avabarre';" value="o projektu..">
+			<span class="slider"></span> 
+		</div>
+	</div>
+	<div class="article-html" data-article="article-2">
+		<img src="/wp-content/themes/pureweb/projects/vaseortopedie.png">
+	</div>
+	<div class="article-html" data-article="article-3">
+		<img src="/wp-content/themes/pureweb/projects/studio_lenky_krejcove.png">
+	</div>
+	 <div class="article-html" data-article="article-4">
+		<img src="/wp-content/themes/pureweb/projects/glasstyle_web.png">
+	</div>		
+	<div class="article-html" data-article="article-5">
+		<img src="/wp-content/themes/pureweb/projects/jakub_komurka.png">
+	</div>
+	<div class="article-html" data-article="article-6">
+		<img src="/wp-content/themes/pureweb/projects/likema.png">
+	</div>
+
+  </section>
+</section>
+<!-- END projects -->
+
+<!-- ABOUT -->
+<section id="kdo_jsem" class="container--full about">
+    <div class="box" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="1000">
+		<div data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-duration="2000">
+			<h1>Pár vět o mně..</h1>
+			<div class="line"></div>
+			<p class="text"><span>Na svých projektech mám ráda příběhy lidí,</span> kteří si budují od základů svoji vizi a společně ji posouváme dál.</p>
+			<p>Jsem web developer a designer - jsem vám nápomocna v celém procesu budování webu. Nepřijímám pouze zadání, ale konzultuji a dávám podněty.</p>
+			<p class="paragraph">Webovému vývoji a designu se věnuji řadu let. Projekty vnímám ze všech úhlů a vytvářím web tak, aby byl přehledný a uživatelsky přívětivý. Nechávám vyniknout vaší službu/produkt.</p>
+			<p class="paragraph">Předchozí pracovní zkušenosti z obchodní oblasti využívám k tomu, aby vám web přinesl i potenciální klienty, protože obsah prodává. </p>
+		</div>
+		<img src="/wp-content/themes/pureweb/img/me_about.png">
+	</div>
+	<div class="parallax" data-parallax-image="/wp-content/themes/pureweb/img/shadow.png"></div>
+
+</section>
+
+
+
+           
+<!-- END about-->
+
+<!-- INFO TEXT -->
+<section id="why_web" class="info_screen">
+	<div class="parallax">
+
+	<div class="info why_web">
+		<h2>| Každý business potřebuje webové stránky</h2>
+		<h3 class="m_4">Tím si budujete svojí</h3>
+				<div class="flexbox">
+					<div class="box">
+						<h3>ZNAČKU</h3>
+						<h3 class="l_01">Z</h3>
+						<p>Představení váší značky potenciálním zákazníkům je jednou z nejdůležitějších věcí, kterou můžete udělat. Jasným stanovením toho, kdo jste, co děláte, co zastupujete a za čím stojíte. Tím zvýšíte šance, že vaši zákazníci u vás nakoupí nebo využijí vašich služeb</p><p style="text-align: left;">-- odlišit se od konkurence</p>					
+					</div>
+					<div class="box">
+						<h3>DŮVĚRYHODNOST</h3>
+						<h3 class="l_02">D</h3>
+						<p>Jedním z hlavních důvodů, proč byste měli mít web pro své podnikání, je vytvořit důvěryhodnost vaší značce. To znamená, mít webové stránky, kde sdělujete kvalitní informace a navíc vypadají dobře - to vzbuzuje důvěru. Web vám umožní vyniknout, díky tomu máte větší šanci získat nové klienty</p><p style="text-align: left;">-- kvalitní informace</p>
+					</div>
+					<div class="box">
+						<h3>DOHLEDATELNOST</h3>
+						<h3 class="l_03">D</h3>
+						<p>Jakmile jste online a máte optimalozovaný web pro SEO (optimalizace nalezitelnosti ve vyhledávačích), objevujete se na důežitých místech ve vyhledávání google. To znamená, že když lidé hledají produkt nebo službu, existuje šance, že se váš web zobrazí ve výsledcích</p><p style="text-align: left;">-- rozšíření portfolia</p>
+					</div>
+					<div class="box">
+						<h3>PŘÍLEŽITOSTI</h3>
+						<h3 class="l_04">P</h3>
+						<p>Snad jeden z nejzajímavějších důvodů, proč mít webové stránky pro svou firmu, je ten, že můžete zvýšit vaše šance na získání potenciálních klientů. Jakmile vás lidé najdou online, začnou se zajímat o váš produkut/službu díky informacím na vašem webu</p><p style="text-align: left;">-- zvýšení prodeje</p>
+					</div>
+			</div>
+		</div>
+	</div>
+</section>
+	<!-- mob screen only -->
+	<section class="info_screen_mob">
+		<div class="info why_web">
+			<h2>| Každý business potřebuje webové stránky</h2>
+			<h3 class="m_4">Tím si budujete svojí</h3>
+			<div class="box read">
+				<h3>ZNAČKU</h3>
+				<h3 class="l_01">Z</h3>
+				<p>Představení váší značky potenciálním zákazníkům je jednou z nejdůležitějších věcí, kterou můžete udělat. Jasným stanovením toho, kdo jste, co děláte, co zastupujete a za čím stojíte. Tím zvýšíte šance, že vaši zákazníci u vás nakoupí nebo využijí vašich služeb</p><p style="text-align: left;">-- odlišit se od konkurence</p>					
+				<p class="read-more"><a href="#" class="btn btn-danger"><img src="/wp-content/themes/pureweb/img/more_dots.svg" alt="číst dál"/></a></p>
+			</div>
+			<div class="box read">
+				<h3>DŮVĚRYHODNOST</h3>
+				<h3 class="l_02">D</h3>
+				<p>Jedním z hlavních důvodů, proč byste měli mít web pro své podnikání, je vytvořit důvěryhodnost vaší značce. To znamená, mít webové stránky, kde sdělujete kvalitní informace a navíc vypadají dobře - to vzbuzuje důvěru. Web vám umožní vyniknout, díky tomu máte větší šanci získat nové klienty</p><p style="text-align: left;">-- kvalitní informace</p>
+				<p class="read-more"><a href="#" class="btn btn-danger"><img src="/wp-content/themes/pureweb/img/more_dots.svg" alt="číst dál"/></a></p>
+			</div>
+			<div class="box read">
+				<h3>DOHLEDATELNOST</h3>
+				<h3 class="l_03">D</h3>
+				<p>Jakmile jste online a máte optimalozovaný web pro SEO (optimalizace nalezitelnosti ve vyhledávačích), objevujete se na důežitých místech ve vyhledávání google. To znamená, že když lidé hledají produkt nebo službu, existuje šance, že se váš web zobrazí ve výsledcích</p><p style="text-align: left;">-- rozšíření portfolia</p>
+				<p class="read-more"><a href="#" class="btn btn-danger"><img src="/wp-content/themes/pureweb/img/more_dots.svg" alt="číst dál"/></a></p>
+			</div>
+			<div class="box read">
+				<h3>PŘÍLEŽITOSTI</h3>
+				<h3 class="l_04">P</h3>
+				<p>Snad jeden z nejzajímavějších důvodů, proč mít webové stránky pro svou firmu, je ten, že můžete zvýšit vaše šance na získání potenciálních klientů. Jakmile vás lidé najdou online, začnou se zajímat o váš produkut/službu díky informacím na vašem webu</p><p style="text-align: left;">-- zvýšení prodeje</p>
+				<p class="read-more"><a href="#" class="btn btn-danger"><img src="/wp-content/themes/pureweb/img/more_dots.svg" alt="číst dál"/></a></p>
+			</div>
+		</div>
+	</section>
+<!-- END info text -->
+
+<!-- SERVICES -->
+<section id="sluzby" class="services">
+
+	<!-- SIDE TITLE-->
+		<h1>SLUŽBY</h1>
+		<h1 class="big_letter">S</h1>
+	<!-- END side title-->
+
+		<div class="text parallax">
+				<h2>webová prezentace</h2>
+				<p>tvorba webu na míru</p>
+
+				<h2>e-commerce | e-shop</h2>
+				<p>platforma shoptet.cz</p>
+				<p>úprava šablony na míru</p>
+
+				<h2>správa webu</h2>
+				<p>veškerou technickou správu nechte na mě</p>
+
+				<h2>Branding | grafický design</h2>
+				<p>paleta barev, návrhy log, ikony, typografieloga, vizitky, vouchery,..</p>				
+
+		</div>
+</section>
+<!-- END services -->
+
+<!-- INFO TEXT -->
+<section id="why_portfolio">
+	<div class="parallax">
+		<div class="info why_portfolio">
+			<div class="portfolio">
+				<h2>| Portfolio na vašem webu je důležité..</h2>
+				<p>prezentujete vaši profesionalitu, oddanost vaší práce a říká o vás mnohem více než jakýkoli životopis. Ukazujete skutečné případy a příklady, které mluví za mnohé. Navíc díky webu portfolia, můžete ukázat svůj vyzrálý a sofistikovaný přístup k prodeji své práce.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END info text -->
+
+<!-- INSTAGRAM -->
+</div>
+<section id="instagram" class="insta">
+
+		<a href="https://www.instagram.com/pureweb.cz/" target="_blank"><h1 class="title">insta</h1></a>
+		<div class="border">
+		<div class="slide">
+			<!-- LightWidget WIDGET -->
+			<script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+			<iframe src="https://cdn.lightwidget.com/widgets/ea753ef7264a55de92aaef70539a45a7.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+		</div>
+			
+</section>
+
+<!-- END instagram -->
+
+<!-- CONTACT -->
+<section id="kontakt" class="contact">
+ <h1>Kontakt</h1>
+<div class="box">
+	<p>Martina Kocmanová</p>
+	<p><a href="mailto:martina@pureweb.cz">&#109;&#97;&#114;&#116;&#105;&#110;&#97;&#64;&#112;&#117;&#114;&#101;&#119;&#101;&#98;&#46;&#99;&#122;</a>
+	<a href="tel:420608738894"> +420 608 738 894</a></p>
+	<p>IČ: 87469936</p>
+	<h2>PureWeb</h2>
+</div>
+	<a href="#home">
+ 		<div class="top">
+				 <p style="transform: scale(0.990833, 0.990833) translate(-0.134146px, -2.86796px) rotate(90deg);
+				 font-size: 0.7rem;">NAHORU</p>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 94 34"><path d="M9 20l1 1h68v4l1 1h1l5-6v-1l-5-5h-1l-1 1v4H10l-1 1z"></path></svg>
+		</div>
+	</a>
+
+</section>
+<!-- END contact -->
+
+<?php get_footer() ?>
